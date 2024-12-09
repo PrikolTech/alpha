c5930491f829f108cae27ec6450f43b34304b405
George Pogrebnoi <gosha1416@yandex.ru>
2024-12-09 14:57:35 +0300

    PA-26: remove extra files

--- backend/core/migrations/psql/001_create_user_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS _user (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    email VARCHAR(255) UNIQUE NOT NULL,
-    first_name VARCHAR(255) NOT NULL,
-    middle_name VARCHAR(255),
-    last_name VARCHAR(255)
-);
