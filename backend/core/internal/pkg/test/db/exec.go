8a8c16cfd053bcea18f5540112a71b1439e903da
Vladimir <74735618+qsoulior@users.noreply.github.com>
2025-01-13 22:01:21 +0300

    PA-40: merge exec functions to one file in test/db

--- backend/core/internal/pkg/test/db/exec.go
+++ backend/core/internal/pkg/test/db/exec.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"slices"
 
+	sq "github.com/Masterminds/squirrel"
 	"github.com/samber/lo"
 )
 
@@ -96,4 +97,68 @@ func InsertEntityWithId[T, U any](c *Container, table string, entity T) (U, erro
 // InsertEntityWithIdCol inserts entity into table and returns id from column idCol.
 func InsertEntityWithIdCol[T, U any](c *Container, table string, entity T, idCol string) (U, error) {
 	return insertEntity[T, U](c, table, entity, idCol, false)
+}
+
+// DeleteEntitiesByIdCol deletes entities from table by ids in column idCol.
+func DeleteEntitiesByIdCol[U any](c *Container, table string, idCol string, ids []U) error {
+	builder := c.builder.
+		Delete(table).
+		Where(sq.Eq{idCol: ids})
+
+	query, args, err := builder.ToSql()
+	if err != nil {
+		return fmt.Errorf("failed to build query for '%s': %w", table, err)
+	}
+
+	_, err = c.db.Exec(query, args...)
+	if err != nil {
+		return fmt.Errorf("failed to exec query for '%s': %w", table, err)
+	}
+
+	return nil
+}
+
+// DeleteEntitiesById deletes entities from table by ids in column "id".
+func DeleteEntitiesById[U any](c *Container, table string, ids []U) error {
+	return DeleteEntitiesByIdCol(c, table, "id", ids)
+}
+
+// DeleteEntityByIdCol deletes entity from table by id in column idCol.
+func DeleteEntityByIdCol[U any](c *Container, table string, idCol string, id U) error {
+	return DeleteEntitiesByIdCol(c, table, idCol, []U{id})
+}
+
+// DeleteEntityById deletes entity from table by id in column "id".
+func DeleteEntityById[U any](c *Container, table string, id U) error {
+	return DeleteEntitiesById(c, table, []U{id})
+}
+
+// SelectEntitiesByIdCol selects entities from table by ids in column idCol
+func SelectEntitiesByIdCol[T, U any](c *Container, table string, idCol string, ids []U) ([]T, error) {
+	var entity T
+	fields := toFields(entity)
+	keys := lo.Map(fields, func(item Field, _ int) string { return item.Key })
+
+	builder := c.builder.
+		Select(keys...).
+		From(table).
+		Where(sq.Eq{idCol: ids})
+
+	query, args, err := builder.ToSql()
+	if err != nil {
+		return nil, fmt.Errorf("failed to build query for '%s': %w", table, err)
+	}
+
+	var entities []T
+	err = c.db.Select(&entities, query, args...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to exec query for '%s': %w", table, err)
+	}
+
+	return entities, nil
+}
+
+// SelectEntitiesById selects entities from table by ids in column "id".
+func SelectEntitiesById[T, U any](c *Container, table string, ids []U) ([]T, error) {
+	return SelectEntitiesByIdCol[T, U](c, table, "id", ids)
 }
